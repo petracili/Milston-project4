ca185224f93f428eb5e5a9dd26cbed0b8c5ed939
petracili <petracili7@gmail.com>
2021-12-26 16:45:06 +0000

    Add prefill form functionality to checkout

---
+++ checkout/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from .models import Order, OrderLineItem
+
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total', 'original_bag',
+                       'stripe_payment_intent_id')
+
+    fields = ('order_number', 'user_profile', 'date', 'first_name',
+              'last_name', 'email', 'phone_number',
+              'street_address1', 'street_address2',
+              'town_or_city', 'postcode', 'county',
+              'country', 'delivery_cost', 'order_total',
+              'grand_total',)
+
+    list_display = ('order_number', 'date', 'first_name', 'last_name',
+                    'order_total', 'delivery_cost', 'grand_total',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
