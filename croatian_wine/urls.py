d5a7b3dc0c40d5543c9721c2bf705bf9c0ac2d49
petracili <petracili7@gmail.com>
2021-12-27 13:07:34 +0000

    Added maine urls for bag

--- croatian_wine/urls.py
+++ croatian_wine/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('profiles/', include('profiles.urls')),
+    path('bag/', include('bag.urls')),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
