703b9b8859e82fb2f14e253ab95de662434f4731
petracili <petracili7@gmail.com>
2021-12-27 12:17:55 +0000

    Add handling for submitted orders and checkout success view

---
+++ bag/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect, reverse, HttpResponse
+
+
+def view_bag(request):
+    """ A view that renders the bag contents page """
+
+    return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
+
+
+def update_bag(request, item_id):
+    """ Update a quantity of the specified product on click """
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop[item_id]
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+def delete_item(request, item_id):
+    """ Delete a product from the bag  """
+
+    bag = request.session.get('bag', {})
+
+    bag.pop[item_id]
+
+    request.session['bag'] = bag
+    return HttpResponse(status=200)
