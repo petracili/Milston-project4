912fdc0fdbf46b5f8dae2d98c167db329fb6a09c
petracili <petracili7@gmail.com>
2021-12-28 22:13:48 +0000

    Change bucket name

--- croatian_wine/settings.py
+++ croatian_wine/settings.py
@@ -183,7 +183,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'myston-project-4'
+    AWS_STORAGE_BUCKET_NAME = 'the-croatian-wine'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
